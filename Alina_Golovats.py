df2e42ae6b44c52e80f9fcc838290d2ca1be1ee7
opilane <opilane@HP-CZC2349HTY>
2023-12-11 10:27:12 +0200

    Add project files.

---
+++ Alina_Golovats.py
@@ -0,0 +1,19 @@
+arv1=float(input("Arv1: "))
+t=input("Tehe: ")
+arv2=float(input("Arv2: "))
+vastus=eval(str(arv1)+t+str(arv2)) #читает в формате текст, а выдаст в формате результата и преобразует
+print(str(arv1)+t+str(arv2)+"="+str(vastus))
+print(arv1,t,arv2,"=",vastus,sep="")
+print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
+
+
+
+print("Tere maailm!".center(75,"-"))  #текст по центру, то что в "" знаки которые будут заполнять пустоту либо пробел
+nimi=input("Mis on sinu nimi?").capitalize() #alina->Alina - формат для большой буквы сначала по умолчанию
+print("Tere "+nimi+"!")
+print("Tere" ,nimi+"!")
+print("Tere {0}!".format(nimi))
+vanus=int(input("Kui vana sa oled?"))        #"21"!=21
+print("Tere {0}! Sa oled {1} aastat vana".format(nimi,vanus))
+print("Muutuja nimi=",nimi,type(nimi)) #обозначение класса переменной вместе с ее значением
+print("Muutuja vanus=",vanus,type(vanus)) #обозначение переменной без ее значения
