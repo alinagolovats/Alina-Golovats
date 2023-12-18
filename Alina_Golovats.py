1569c82330f5d7ea1e636081bda9771fdfe4d835
opilane <opilane@HP-CZC2349HTY>
2023-12-18 11:40:48 +0200

    .

--- Alina_Golovats.py
+++ Alina_Golovats.py
@@ -3,112 +3,113 @@ from random import randint
 from stringprep import c22_specials, c6_set
 
 
-#arv1=float(input("Arv1: "))
-#t=input("Tehe: ")
-#arv2=float(input("Arv2: "))
-#vastus=eval(str(arv1)+t+str(arv2)) #читает в формате текст, а выдаст в формате результата и преобразует
-#print(str(arv1)+t+str(arv2)+"="+str(vastus))
-#print(arv1,t,arv2,"=",vastus,sep="")
-#print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
+arv1=float(input("Arv1: "))
+t=input("Tehe: ")
+arv2=float(input("Arv2: "))
+vastus=eval(str(arv1)+t+str(arv2)) #читает в формате текст, а выдаст в формате результата и преобразует
+print(str(arv1)+t+str(arv2)+"="+str(vastus))
+print(arv1,t,arv2,"=",vastus,sep="")
+print("{0}{1}{2}={3}".format(arv1,t,arv2,vastus))
 
 
 
-#nimi=input("Mis on sinu nimi?").capitalize() #alina->Alina - формат для большой буквы сначала по умолчанию
-#print("Tere "+nimi+"!")#print("Tere maailm!".center(75,"-"))  #текст по центру, то что в "" знаки которые будут заполнять пустоту либо пробел
-#print("Tere" ,nimi+"!")
-#print("Tere {0}!".format(nimi))
-#vanus=int(input("Kui vana sa oled?"))        #"21"!=21
-#print("Tere {0}! Sa oled {1} aastat vana".format(nimi,vanus))
-#print("Muutuja nimi=",nimi,type(nimi)) #обозначение класса переменной вместе с ее значением
-#print("Muutuja vanus=",vanus,type(vanus)) #обозначение переменной без ее значения
+nimi=input("Mis on sinu nimi?").capitalize() #alina->Alina - формат для большой буквы сначала по умолчанию
+print("Tere "+nimi+"!")#print("Tere maailm!".center(75,"-"))  #текст по центру, то что в "" знаки которые будут заполнять пустоту либо пробел
+print("Tere" ,nimi+"!")
+print("Tere {0}!".format(nimi))
+vanus=int(input("Kui vana sa oled?"))        #"21"!=21
+print("Tere {0}! Sa oled {1} aastat vana".format(nimi,vanus))
+print("Muutuja nimi=",nimi,type(nimi)) #обозначение класса переменной вместе с ее значением
+print("Muutuja vanus=",vanus,type(vanus)) #обозначение переменной без ее значения
 
 
-#vanus=18
-#eesnimi="Jaak"
-#pikkus=16.5
-#kas_käib_koolis=True 
+vanus=18
+eesnimi="Jaak"
+pikkus=16.5
+kas_käib_koolis=True 
 
-#print("Muutuja vanus=",vanus,"on",type(vanus))
-#print("Muutuja eesnimi=",eesnimi,"on",type(eesnimi))
-#print("Muutuja pikkus=",pikkus,"on",type(pikkus))
-#print("Muutuja kas_käib_koolis=",kas_käib_koolis,"on",type(kas_käib_koolis))
-
-
-#from random import *
-#from math import *
-#kokku=randint(1,100)
-#print("Laua peal on",kokku,"kommid. Mitu tahad süüa?")
-#kommid=int(input())
-#kokku-=kommid
-#print("Nüüd kokku on",kokku)
+print("Muutuja vanus=",vanus,"on",type(vanus))
+print("Muutuja eesnimi=",eesnimi,"on",type(eesnimi))
+print("Muutuja pikkus=",pikkus,"on",type(pikkus))
+print("Muutuja kas_käib_koolis=",kas_käib_koolis,"on",type(kas_käib_koolis))
 
 
-#u=float(input("Ümbermõõt: ")) #L=pi*2*r=pi*d
-#d=round(u/pi,2)
-#print("Läbimõõt =",d)
+from random import *
+from math import *
+kokku=randint(1,100)
+print("Laua peal on",kokku,"kommid. Mitu tahad süüa?")
+kommid=int(input())
+kokku-=kommid
+print("Nüüd kokku on",kokku)
 
 
-#N=float(input("Sisesta ristküliku laius (N meetrites): "))
-#M=float(input("Sisesta ristküliku pikkus (M meetrites: "))
-#d=sqrt(N**+M**2)
-#print("Diagonaal=",d,"m")
+u=float(input("Ümbermõõt: ")) #L=pi*2*r=pi*d
+d=round(u/pi,2)
+print("Läbimõõt =",d)
 
 
-#try:
-#    aeg = float(input("Mitu tundi kulus sõiduks? "))
-#except :
-#    print("On vaja ainult numbrid sisastada")
-#    try:
-#        teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
-#        kiirus = teepikkus/aeg
-#        print("Sinu kiirus oli " + str(kiirus) + " km/h")
-#    except :
-#        print("Viga andmetaga")
-#aeg = float(input("Mitu tundi kulus sõiduks? "))
-#teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
-#kiirus = teepikkus / aeg
-#print("Sinu kiirus oli " + str(kiirus) + " km/h")
+N=float(input("Sisesta ristküliku laius (N meetrites): "))
+M=float(input("Sisesta ristküliku pikkus (M meetrites: "))
+d=sqrt(N**+M**2)
+print("Diagonaal=",d,"m")
 
 
-#try:
-#    min_=int(input("Min: "))
-#except :
-#    print("On vaja täisarv kasutada!")
-#try:
-#    max_=int(input("Max: "))
-#except :
-#    print("Viga max_ muutujaga!")
+try:
+    aeg = float(input("Mitu tundi kulus sõiduks? "))
+except :
+    print("On vaja ainult numbrid sisastada")
+    try:
+        teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+        kiirus = teepikkus/aeg
+        print("Sinu kiirus oli " + str(kiirus) + " km/h")
+    except :
+        print("Viga andmetaga")
+aeg = float(input("Mitu tundi kulus sõiduks? "))
+teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+kiirus = teepikkus / aeg
+print("Sinu kiirus oli " + str(kiirus) + " km/h")
 
 
-#a1=randint(min_,max_)
-#a2=randint(min_,max_)
-#a3=randint(min_,max_)
-#a4=randint(min_,max_)
-#a5=randint(min_,max_)
-#keskmine=(a1+a2+a3+a4+a5)/5
-#print("Arvud: {0},{1},{2},{3},{4}. Aritmetiline keskmine on: {5}".format(a1,a2,a3,a4,a5,keskmine))
+try:
+    min_=int(input("Min: "))
+except :
+    print("On vaja täisarv kasutada!")
+try:
+    max_=int(input("Max: "))
+except :
+    print("Viga max_ muutujaga!")
 
 
-#print("@..@".center(10," "))
-#print("(----)".center(10," "))
-#print("( \__/ )".center(10," "))
-#print('^^ "" ^^'.center(10," "))
+a1=randint(min_,max_)
+a2=randint(min_,max_)
+a3=randint(min_,max_)
+a4=randint(min_,max_)
+a5=randint(min_,max_)
+keskmine=(a1+a2+a3+a4+a5)/5
+print("Arvud: {0},{1},{2},{3},{4}. Aritmetiline keskmine on: {5}".format(a1,a2,a3,a4,a5,keskmine))
 
 
-#try:
-#    a=float(input("a: "))
-#except :
-#    print("On vaja täisarv kasutada!")
-#try:
-#    b=float(input("b: "))
-#except :
-#    print("Viga muutujaga!")
-#try:
-#    c=float(input("c: "))
-#except :
-#    print("Viga c muutujaga!")
-#P=a+b+c
-#print("Perimetr/Ümbermõõt: ",P)
+print("@..@".center(10," "))
+print("(----)".center(10," "))
+print("( \__/ )".center(10," "))
+print('^^ "" ^^'.center(10," "))
+
+
+try:
+    a=float(input("a: "))
+except :
+    print("On vaja täisarv kasutada!")
+try:
+    b=float(input("b: "))
+except :
+    print("Viga muutujaga!")
+try:
+    c=float(input("c: "))
+except :
+    print("Viga c muutujaga!")
+P=a+b+c
+print("Perimetr/Ümbermõõt: ",P)
+
 
 try:
     P=int(input("Sõbrade kogus: "))
---
+++ korduslaused.py
@@ -0,0 +1,44 @@
+#arv=int(input("Sisesta arv: "))
+#if arv==13:
+#    arv=77
+#print(arv)
+
+
+#if arv>0:
+#    print("Positiivne arv")
+#elif arv<0:
+#    print("Negatiivne arv")
+#else:
+#    print("Ei saa arvestada")
+
+
+#hinne=input("Mis hinne sa täna sai?") #A MA X
+#if hinne=="5" or hinne=="A":
+#    print("Tubli!")
+#elif hinne=="4":
+#    print("Hea hinne!")
+#elif hinne=="3":
+#    print("Rahuldav!")
+#elif hinne=="2" or hinne=="1" or hinne=="MA":
+#    print("Halb!")
+#elif hinne=="X":
+#    print("On vaja töö soritada")
+#else:
+#    print("Ma ei tea, mis hinne see on!")
+
+
+#from random import *
+#a=randint(-1000,1000)
+#print(a)
+#if a%3==0:
+#    print("{0} jagatakse kolmega".format(a))
+#else:
+#    print("Ei saa jagada kolmega")
+
+from random import *
+vanus=randint(14,23)
+print(vanus)
+if vanus>=18:
+    print("Täiskasvanud inimene. Vanemad ei saa kontrollida tulemusi")
+else:
+    print("Vanemad kontrollivad lapsi")
