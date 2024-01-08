e0421497daf7cc2fc39a78f304fafb799545d864
opilane <opilane@HP-CZC2349HTY>
2024-01-08 11:39:40 +0200

    prakt

--- praktilinetöö.py
+++ praktilinetöö.py
@@ -52,14 +52,74 @@
 #    print("Viga")
   
 
-try:
-    hind=float(input("Sisesta hind: "))
-    if hind>=700:
-       hind*=0.7
-       print(hind)
-    else:
-        print(hind)
-except:
-    print("Viga")
+#try:
+#    hind=float(input("Sisesta hind: "))
+#    if hind>=700:
+#       hind*=0.7
+#       print(hind)
+#    else:
+#       print(hind)
+#except:
+#    print("Viga")
 
+
+#sugu=input("Kas sa oled mees või naine?").lower()
+#if sugu=="naine" or sugu=="n":
+#    l1=155
+#    l2=170
+#    l3=255
+#elif sugu=="mees" or sugu=="m":
+#    l1=160
+#    l2=180
+#    l3=255
+#else:
+#    l1=0
+#    print("Viga")
+#if l1!=0:
+#  try:
+#    pikkus=int(input("Sisesta oma pikkus"))
+#    if pikkus>29 and pikkus<155: #100
+#        vastus="lühike"
+#    elif pikkus>=l1 and pikkus<l2: #165
+#        vastus="keskmine"
+#    elif pikkus>=l2 and pikkus<=l3: #200
+#        vastus="pikk"
+#    else:
+#        vastus="tundmatu"
+#    print("Sinu pikkus on {0}" .format(vastus))
+#  except:
+#    print("Vale andmete formaat!")
+
+
+from datetime import *
+from random import *
+arve_nr=datetime.now() #date.today()
+print(arve_nr)
+tsekk="Arve: "+str(arve_nr)+"\nToode Hind Kogus Summa/n"
+summa=0
+toode1="piim"
+hind=randint(50,150)/100
+v=input("Toode:"+toode1+" Hind:"+str(hind)+"\nKas tahad osta?").lower()
+if v=="jah":
+    mitu=int(input("Mitu?"))
+    tsekk+=toode1+" "+str(hind)+" "+str(mitu)+" "+str(mitu*hind)+"/n"
+    summa+=mitu*hind
+print(tsekk)
+toode2="mahl"
+hind=randint(50,150)/100
+v=input("Toode:"+toode2+" Hind:"+str(hind)+"\nKas tahad osta?").lower
+if v=="jah":
+    mitu=int(input("Mitu?"))
+    tsekk+=toode1+" ",+str(hind)," ",+str(mitu)+" "+str(mitu*hind)+"\n"
+    summa+=mitu*hind
+print(tsekk)
+toode3="vesi"
+hind=randint(50,150)/100
+v=input("Toode:"+toode3+" Hind:"+str(hind)+"\nKas tahad osta?").lower
+if v=="jah":
+    mitu=int(input("Mitu?"))
+    tsekk+=toode3+" ",+str(hind)," ",+str(mitu)+" "+str(mitu*hind)+"\n"
+    summa+=mitu*hind
+tsekk+="Kokku maksta: "+str(summa)
+print(tsekk)
 
