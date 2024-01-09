3bdab63f827fc8652c1f274be2915ebd4deaf1be
opilane <opilane@HP-CZC2349HTY>
2024-01-09 10:40:36 +0200

    tgttg

--- praktilinetöö.py
+++ praktilinetöö.py
@@ -123,3 +123,99 @@
 #tsekk+="Kokku maksta: "+str(summa)
 #print(tsekk)
 
+#try:
+#    gender=input("Kas sa oled mees või naine?")
+#    if gender.lower()=="naine":
+#        print("Kahjuks, otsime ainult mehi")
+#    else:
+#        age=int(input("Palun märkige oma vanus"))
+#    if age>=16 and age<=18:
+#        print("Sa sobid meie meeskonda!")
+#    else:
+#        print("Kahjuks sa ei sobi meie meeskonda")
+#except:
+#    print("Viga")
+
+#try:
+#    hind=float(input("Sisesta toote hind: "))
+#    if hind<=10:
+#        soodustus=hind*0.1
+#    else:
+#        soodustus=hind*0.2
+#        okonnelik_hind=hind-soodustus
+#        print("Lõplik hind on",okonnelik_hind,"€")
+#except:
+#    print("Viga")
+
+#from math import*
+#try:
+#    s1=float(input("Введите длину первой стороны квадрата: "))
+#    s2=float(input("Введите длину второй стороны квадрата: "))
+#    if s1==s2:
+#        print("Это квадрат")
+#    else:
+#        print("Это не квадрат")
+#except:
+#    print("Где то ошибка посмотрите тип как вы указали данные")
+
+
+#a=10 #int
+#b=2.3 #float
+#c="programma" #str
+#d="proga1" #str
+#print(b.is_integer()) #false
+#print(c.isalpha()) #true
+#print(d.isalpha()) #false
+#print(d.isnumeric()) #false
+#print(d.isdigit()) #false 
+
+
+#try:
+#    gender=input("Sugu: ")
+#    if gender.isalpha() and (gender.lower()=="naine" or gender.lower()=="mees"):
+#        if gender.lower()=="naine":
+#            print("Ei soobi")
+#        else:    
+#            try:
+#                age=int(input("Vanus: "))
+#                if 16<=age<=18:
+#                    print("Oled meeskonnas")
+#                else:
+#                    print("Vanus ei soobi")
+#            except:
+#                print("Vale vanus! Viga andmetüübiga")
+#    else:
+#        print("Sisesta tekst")
+#except:
+#    print("Viga andmetüübiga")
+
+
+try:
+    print("Tere! Olen sinu uus sõber - Python!")
+    nimi = input("Sisestage oma nimi: ")
+    print(nimi + ", oi kui ilus nimi!")
+    vastus = int(input(nimi + "! Kas leian Sinu keha indeksi? 0-ei, 1-jah => "))
+    if vastus == 1:
+        pikkus = int(input("Sisestage oma pikkus (cm): "))
+        mass = float(input("Sisestage oma mass (kg): "))
+        indeks = mass / ((0.01 * pikkus) ** 2)
+        print(nimi + "! Sinu keha indeks on: {:.1f}".format(indeks))
+        if indeks < 16:
+            print("Tervisele ohtlik alakaal")
+        elif 16 <= indeks < 19:
+            print("Alakaal")
+        elif 19 <= indeks < 25:
+            print("Normaalkaal")
+        elif 25 <= indeks < 30:
+            print("Ülekaal")
+        elif 30 <= indeks < 35:
+            print("Rasvumine")
+        elif 35 <= indeks < 40:
+            print("Tugev rasvumine")
+        else:
+            print("Tervisele ohtlik rasvumine")
+    else:
+        print("Kahju! See on väga kasulik info!")
+    print("\nKohtumiseni, " + nimi + "! Igavesti Sinu, Python!")
+except ValueError:
+    print("Viga! Palun sisestage õige arvuline väärtus.")
